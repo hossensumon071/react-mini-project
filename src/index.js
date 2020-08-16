d7ea2272e05ae6a8e00768bc2cdcaf2287e60f98
hossensumon071 <hossensumon071@gmail.com>
2020-08-16 10:31:24 +0600

    creted books components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Fragment} from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function Books() {
+  return(
+    <>
+      <div>
+        <Book/>
+        <Book/>
+      </div>
+    </>
+  );
+}
+const Book = () => {
+  return <h1>this is a book</h1>;
+};
+
+ReactDom.render(<Books/>,document.getElementById('root'));
