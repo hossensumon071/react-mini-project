579b617382fc444ca298e2a7a3034059e11966f7
hossensumon071 <hossensumon071@gmail.com>
2020-08-18 15:34:47 +0600

    created book component

--- src/index.js
+++ src/index.js
@@ -9,12 +9,31 @@ function Books() {
       <div>
         <Book/>
         <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
       </div>
     </>
   );
 }
 const Book = () => {
-  return <h1>this is a book</h1>;
+  return (
+    <article>
+      <CoverImage/>
+      <Title/>
+      <Author/>
+    </article>
+  )
 };
+
+const CoverImage = () =>
+ <img 
+ width="200"
+ src="https://images-na.ssl-images-amazon.com/images/I/41aIidpbKwL._AC_SX184_.jpg"
+  alt="wonky donky"
+  />
+
+const Title = () => <h1>The Wonkey Donky</h1>
+const Author = () => <p>by Craing Smith</p>
 
 ReactDom.render(<Books/>,document.getElementById('root'));
